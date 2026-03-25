650ae9b8049beb4efe8e99cb1fabb8681283b122
esantos2019254 <esantos-2019254@kinal.edu.gt>
2026-03-25 13:15:07 -0600

    feat: usuarios

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -5,6 +5,29 @@ export const Contador = () => {
 
     const [contador, setContador] = useState(0);
 
+    return (
+        <div>
+            <h2> Contador: {contador}</h2>
+
+            <button onClick={() => setContador(contador + 1)}>
+                Incrementar
+            </button>
+
+            <button onClick={() => setContador(contador - 1)}>
+                Disminuir
+            </button>
+        </div>
+    )
+}
+
+/*
+
+import { useState } from "react"
+
+export const Contador = () => {
+
+    const [contador, setContador] = useState(0);
+
   return (
     <div>
         <h2> Contador: {contador}</h2>
@@ -19,3 +42,4 @@ export const Contador = () => {
     </div>
   )
 }
+*/
